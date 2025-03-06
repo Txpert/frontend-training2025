f03ed3cbffb516060d4eb62bb3fb1bf706902369
Txpert <29957106+Txpert@users.noreply.github.com>
2025-03-06 15:20:20 +0400

    Bauplan

--- JS/5_product_mangement_system/Bauplan.md
+++ JS/5_product_mangement_system/Bauplan.md
@@ -1,2 +1,23 @@
-1- Produkte hinzufügen  
-2- Produkte anzeigen
+1-Alle Produkte speichern
+Arrays
+6
+
+2- Funktion Produkte hinzufügen
+push  
+
+3- Funktion Produkte anzeigen //WARNKORB
+schleifen foreach dom
+InnerHTML
+
+InnerHTML
+
+
+4- Funktion zum Erstellen eines neuen Produkts
+Objekt hat einen namen
+    name
+    price
+    beschreibung
+
+5- Funktion zum Entfernen eines Produkts
+
+6- Event-Listener für das Formular
---
+++ JS/5_product_mangement_system/script.js
@@ -0,0 +1,25 @@
+// speichert alle Produkte
+let produktListe = [];
+
+//Funktion zum Hinzufügen eines Produkts
+function produktHinzufuegen(name, preis, beschreibung){
+
+
+}
+
+// Funktion zum Erstellen eines neuen Produkts
+function estellenProdukt(name, preis, beschreibung) {
+
+}
+
+// Funktion zum Entfernen eines Produkts
+function produktEntfernen(id){
+
+}
+
+// Funktion zum Anzeigen aller Produkte
+function produkteAnzeigen(){
+
+}
+
+// Event-Listener für das Formular
